909be494232d667d8cddfd8ca71cd30256824520
Masataka Marukawa <masataka.marukawa@gmail.com>
2021-03-22 18:26:30 +0900

    Update delete.py

--- delete.py
+++ delete.py
@@ -57,7 +57,73 @@ for region in regions:
 
     config["region"] = region.region_name
 
+    ###Database###
+    database.purge_autonomous_dbs(config, signer, compartments)
+    database.purge_db_systems(config, signer, compartments)
+
+    ###Load Balancer###
+    load_balancer.purge_load_balancers(config, signer, compartments)
+
+    ###Compute & Compute Operation###
+    auto_scaling.purge_auto_scaling_configurations(config, signer, compartments)
+    compute_management.purge_instance_pools(config, signer, compartments)
+    compute_management.purge_instance_configurations(config, signer, compartments)
+    compute.purge_compute_instances(config, signer, compartments)
+    compute.purge_dedicated_vm_hosts(config, signer, compartments)
+    compute.purge_images(config, signer, compartments)
+
+    ###Block Volume###
+    blockstorage.purge_volume_group_backups(config, signer, compartments)
+    blockstorage.purge_volume_groups(config, signer, compartments)
+    blockstorage.purge_volume_backup_policies(config, signer, compartments)
+    blockstorage.purge_volume_backups(config, signer, compartments)
+    blockstorage.purge_volumes(config, signer, compartments)
+    blockstorage.purge_boot_volume_backups(config, signer, compartments)
+    ##blockstorage.purge_boot_volumes(config, signer, compartments) #AD Required
+
+    ###File Storage###
+    file_storage.purge_exports(config, signer, compartments)
+    file_storage.purge_snapshots(config, signer, compartments)
+    file_storage.purge_file_systems(config, signer, compartments) #AD Required
+    file_storage.purge_mount_targets(config, signer, compartments) #AD Required
+
+    ###Object Storage###
+    object_storage.purge_preauthenticated_requests(config, signer, compartments)
+    object_storage.purge_multipart_uploads(config, signer, compartments)
+    object_storage.purge_objects(config, signer, compartments)
+    object_storage.purge_buckets(config, signer, compartments)
+
+    ###Virtual Network###
+    virtual_network.purge_subnets(config, signer, compartments)
+    virtual_network.clear_route_rules(config, signer, compartments)
+    virtual_network.purge_route_tables(config, signer, compartments)
+    virtual_network.purge_drg_attachments(config, signer, compartments)
+    virtual_network.purge_local_peering_gateways(config, signer, compartments)
+    virtual_network.purge_internet_gateways(config, signer, compartments)
+    virtual_network.purge_nat_gateways(config, signer, compartments)
+    virtual_network.purge_service_gateways(config, signer, compartments)
+    virtual_network.purge_network_security_groups(config, signer, compartments)
+    virtual_network.purge_security_lists(config, signer, compartments)
+    virtual_network.purge_dhcp_options(config, signer, compartments)
+    #virtual_network.purge_ipv6s(config, signer, compartments)
+    #virtual_network.purge_vlans(config, signer, compartments)
+    virtual_network.purge_vcns(config, signer, compartments)
+    virtual_network.purge_remote_peering_connections(config, signer, compartments)
+    virtual_network.purge_ip_sec_connections(config, signer, compartments)
+    #virtual_network.purge_cross_connects(config, signer, compartments)
+    #virtual_network.purge_cross_connect_groups(config, signer, compartments)
+    #virtual_network.purge_virtual_circuits(config, signer, compartments)
+    virtual_network.purge_drgs(config, signer, compartments)
+    virtual_network.purge_cpes(config, signer, compartments)
+    virtual_network.purge_public_ips(config, signer, compartments)
+
     if region.region_name == home_region:
+
+        ###Web Application Firewall###
+        waas.purge_address_lists(config, signer, compartments)
+        waas.purge_certificates(config, signer, compartments)
+        waas.purge_custom_protection_rules(config, signer, compartments)
+        waas.purge_waas_policies(config, signer, compartments)
 
         ###IAM###
         identity.purge_policies(config, signer, compartments)
